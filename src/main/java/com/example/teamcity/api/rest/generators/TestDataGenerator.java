43675574449ba041cb629658a349cd93b139259c
Victor Ivanov <vivanov@allot.com>
2024-09-23 00:59:41 +0300

    module Common class for test data - added test data class - added method to generate test data class - added test data generation into before test method

--- src/main/java/com/example/teamcity/api/rest/generators/TestDataGenerator.java
+++ src/main/java/com/example/teamcity/api/rest/generators/TestDataGenerator.java
@@ -4,9 +4,11 @@ import com.example.teamcity.api.rest.annotations.Optional;
 import com.example.teamcity.api.rest.annotations.Parameterizable;
 import com.example.teamcity.api.rest.annotations.Random;
 import com.example.teamcity.api.rest.models.BaseModel;
+import com.example.teamcity.api.rest.models.TestData;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -77,6 +79,26 @@ public final class TestDataGenerator {
             return instance;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException
                  | NoSuchMethodException e) {
+            throw new IllegalStateException("Cannot generate test data", e);
+        }
+    }
+
+    public static TestData generate() {
+        // Идем по всем полям TestData и для каждого, кто наследник BaseModel вызывыем generate() c передачей уже сгенерированных сущностей
+        try {
+            var instance = TestData.class.getDeclaredConstructor().newInstance();
+            var generatedModels = new ArrayList<BaseModel>();
+            for (var field: TestData.class.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (BaseModel.class.isAssignableFrom(field.getType())) {
+                    var generatedModel = generate(generatedModels, field.getType().asSubclass(BaseModel.class));
+                    field.set(instance, generatedModel);
+                    generatedModels.add(generatedModel);
+                }
+                field.setAccessible(false);
+            }
+            return instance;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new IllegalStateException("Cannot generate test data", e);
         }
     }
--- src/test/java/com/example/teamcity/api/BaseTest.java
+++ src/test/java/com/example/teamcity/api/BaseTest.java
@@ -1,24 +1,29 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.rest.models.TestData;
 import com.example.teamcity.api.rest.requests.CheckedRequests;
 import com.example.teamcity.api.rest.spec.Specifications;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
+import static com.example.teamcity.api.rest.generators.TestDataGenerator.generate;
+
 public class BaseTest {
 
-    protected SoftAssert softAssert;
+    protected SoftAssert softy;
     protected CheckedRequests superUserCheckRequests = new CheckedRequests(Specifications.superUserSpec());
+    protected TestData testData;
 
     @BeforeMethod(alwaysRun = true)
     public void beforeTest() {
-        softAssert = new SoftAssert();
+        softy = new SoftAssert();
+        testData = generate();
     }
 
     @AfterMethod(alwaysRun = true)
     public void afterTest() {
-        softAssert.assertAll();
+        softy.assertAll();
     }
 
 }
